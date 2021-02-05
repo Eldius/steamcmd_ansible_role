7ac72495b7384efc8953f24eb598db0526d37337
Eldio Santos <eldio.santos@m4u.com.br>
2021-02-05 16:11:48 -0300

    Removed the login file (to avoid write password to file)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,9 +7,45 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
+def test_user_is_present(host):
+    u = host.user('steam')
+
+    assert u.exists
+    assert u.name == 'steam'
+    assert u.group == 'steam'
+
+
+def test_install_folder_is_present(host):
     f = host.file('/home/steam/Steam')
 
     assert f.exists
     assert f.user == 'steam'
     assert f.group == 'steam'
+    assert f.is_directory
+
+
+def test_steamcmd_script_is_present(host):
+    f = host.file('/home/steam/Steam/steamcmd.sh')
+
+    assert f.exists
+    assert f.user == 'steam'
+    assert f.group == 'steam'
+    assert f.is_file
+
+
+def test_package_folder_is_present(host):
+    f = host.file('/home/steam/Steam/package')
+
+    assert f.exists
+    assert f.user == 'steam'
+    assert f.group == 'steam'
+    assert f.is_directory
+
+
+def test_userdata_is_present(host):
+    f = host.file('/home/steam/Steam/userdata/anonymous/config')
+
+    assert f.exists
+    assert f.user == 'steam'
+    assert f.group == 'steam'
+    assert f.is_directory
